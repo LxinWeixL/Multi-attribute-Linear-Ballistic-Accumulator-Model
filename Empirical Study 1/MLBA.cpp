05b2cde2df5d8b0fe4dcf134988271884253b3ea
XINWEI LI <134404489+LxinWeixL@users.noreply.github.com>
2026-04-12 00:17:46 -0700

    Update print statement from 'Hello' to 'Goodbye'

--- Empirical Study 1/MLBA.cpp
+++ Empirical Study 1/MLBA.cpp
@@ -319,7 +319,7 @@ double  RCPPcdf_MLBA_CO(arma::mat X, arma::vec beta, arma::vec zeta, double lam1
 	MLBA_CDF f(X, beta,zeta,lam1, lam2, b, s, A, choice) ;
 	double err_est;
     	int err_code;
-	double res = integrate(f, lower, upper, err_est, err_code,100);
+	double res = integrate(f, lower, upper, err_est, err_code,1000);
 	if(err_est>0.1) res = 1e-10;//-1e5-err_est;
 	if(err_code!=0) res = 1e-10;//-1e4-err_code;
 	//if(res<1e-10) res = 1e-10;
@@ -658,7 +658,7 @@ double  derivative_mlba_co_di(arma::mat X, arma::vec beta, arma::vec zeta, doubl
 	MLBA_deri_di f(X, beta,zeta,lam1, lam2, b, s, A,  choice,di) ;
 	double err_est;
     	int err_code;
-	double res = integrate(f, lower, upper, err_est, err_code,100);
+	double res = integrate(f, lower, upper, err_est, err_code,1000);
 	if(err_est>0.01) res = 0;
 	if(err_code!=0) res = 0;
 	return res;
@@ -671,7 +671,7 @@ double  derivative_mlba_co_b(arma::mat X, arma::vec beta, arma::vec zeta, double
 	MLBA_deri_b f(X, beta,zeta,lam1, lam2, b, s, A,  choice) ;
 	double err_est;
     	int err_code;
-	double res = integrate(f, lower, upper, err_est, err_code,100);
+	double res = integrate(f, lower, upper, err_est, err_code,1000);
 	if(err_est>0.01) res = 0;
 	if(err_code!=0) res = 0;
 	return res;
